9671711b56197cbd99c4593af0c693ac025829e9
Harry Marshall <harryyymarshall@gmail.com>
2025-06-12 14:31:41 +1000

    footer change

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -170,7 +170,7 @@ export default function Footer() {
         </div>
 
         <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400 font-light">
-          <p>&copy; 2024 Precision Barber Studio. All rights reserved. | Crafted with precision in Melbourne.</p>
+          <p>&copy; 2025 UR Studio. All rights reserved. | Crafted with precision in Melbourne.</p>
         </div>
       </div>
     </footer>
