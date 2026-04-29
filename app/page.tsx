6d7c48eb04f9f102186e114e61773af2e10ef3cd
Harry Marshall <harryyymarshall@gmail.com>
2026-04-29 10:27:20 +1000

    feat: remove services component on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-white dark:bg-black text-black dark:text-white">
       <Hero />
-      <Services />
+      {/* <Services /> */}
       <Gallery />
       <Stats />
       <About />
